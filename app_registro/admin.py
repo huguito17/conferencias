dd8c5a46421a00f17aca8ca1a1d8b99415ec33bc
Hugo Murillo Urbina <hmurillo464@hotmail.com>
2021-06-29 18:14:20 -0600

    cambios en conferencias, conferensistas, asistencias, entre otros

--- app_registro/admin.py
+++ app_registro/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from .models import Conferencia, Conferencista, Participante
+from .models import Conferencia, Conferencista, Participante, Asistencia
 
 # ------------------------------------------------------------------------
 class ConferenciaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'fecha', 'hora', 'conferencista')
-    list_editable = ('nombre', 'conferencista')
+    list_display = ('id', 'nombre', 'fecha', 'hora', 'estado',)
+    list_editable = ('nombre', 'fecha', 'estado',)
 
 admin.site.register(Conferencia, ConferenciaAdmin)
 # ------------------------------------------------------------------------
 admin.site.register(Conferencista)
 # ------------------------------------------------------------------------
+admin.site.register(Asistencia)
+# ------------------------------------------------------------------------
 admin.site.register(Participante)
+# ------------------------------------------------------------------------
--- app_registro/models.py
+++ app_registro/models.py
@@ -11,14 +11,23 @@ class Conferencista(models.Model):
         return self.nombre
 
 class Conferencia(models.Model):
+    #Dupla de duplas
+    ESTADOS = (
+        ('1', 'Pendiente'),
+        ('2', 'En Proceso'),
+        ('3', 'Finalizada'),
+        ('4', 'Cancelada'),
+    )
     nombre = models.CharField(max_length=35)
     fecha_registro = models.DateTimeField(auto_now_add=True)
     fecha = models.DateField()
     hora = models.TimeField()
-    conferencista = models.ForeignKey(Conferencista, on_delete=models.CASCADE, null=True)
+    conferencista = models.ManyToManyField(Conferencista, blank=True)
+    estado = models.CharField(max_length=1, choices=ESTADOS, default='1')
+    cupos = models.SmallIntegerField(default=10)
 
     def __str__(self):
-        return f'Conferencia; {self.nombre} - Conferencista: {self.conferencista}'
+        return f'Conferencia: {self.nombre}'
         
 class Participante(models.Model):
     nombre = models.CharField(max_length=25)
@@ -28,3 +37,14 @@ class Participante(models.Model):
 
     def __str__(self):
         return f'{self.nombre} {self.apellido}'
+
+class Asistencia(models.Model):
+    conferencia = models.ForeignKey(Conferencia, on_delete=models.CASCADE)
+    participante = models.ForeignKey(Participante, on_delete=models.CASCADE)
+    confirmacion = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.conferencia} | {self.participante}'
+    
+    class Meta:
+        unique_together = ('conferencia', 'participante')
